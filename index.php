b87707298fc07b88f178f8c7dd5c0a0f5ef12874
coderblue10 <produccionesbog@gmail.com>
2023-11-09 17:07:57 -0500

    creacion del portfolio de servicios web

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codernaf | Home</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&family=Rubik:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+
+    <header>
+        <div class="header-container">
+            <img src="./assets/img/CODERNAF-LOGO 1.png" alt="CODERNAF LOGO" class="logo">
+            <div class="menú">
+                <ul>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>services</li>
+                    <li>portfolio</li>
+                    <li>pages</li>
+                </ul>
+                <div>
+                    <p><span></span>search</p>
+                </div>
+            </div>
+            <span class="menu-mobile"></span>
+        </div>
+    </header>
+
+    <!-- section one -->
+    
+    <section class="section-one">
+        <section class="section-one--container">
+            <section  class="section-one-container--content">
+                <img src="./assets/img/circle.svg" alt="" class="section-one-content-text--circle">
+                <picture>
+                    <img src="" alt="" class="none">
+                </picture>
+                <div class="section-one-content--text">
+                    <h1><span>¡Hola! Soy Fabian Silva,</span> tu Experto en Desarrollo Web y Software</h1>
+                    <div>
+                    <h2><span></span>Diseño soluciones digitales innovadoras que integran PHP en el backend y JavaScript en el frontend. Transformo ideas en experiencias web y aplicaciones funcionales. ¡Trabaja conmigo para hacer realidad tu visión!</h2>
+                    </div>
+                    <button class="call-to-action">Hablemos de tu Idea<span></span></button>
+                </div>
+            </section>
+        </section>
+    </section>
+    
+    <main>
+
+        <!-- section two -->
+
+        <section class="section-two">
+            <section class="section-two--container">
+                <div class="section-two-container--content">
+                    <div class="section-two-content--container">
+                        <div class="circle-transition"></div>
+                        <span></span>
+                        <h3>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quisquam, ab deleniti consequuntur eaque odit!</p>
+                    </div>
+                    <div class="section-two-content--container">
+                        <div class="circle-transition"></div>
+                        <span></span>
+                        <h3>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et voluptas sit placeat, maiores natus similique.</p>
+                    </div>
+                    <div class="section-two-content--container">
+                        <div class="circle-transition"></div>    
+                        <span></span>
+                        <h3>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium quisquam debitis animi quam accusamus quasi?</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+    </main>
+    
+    
+</body>
+</html>
